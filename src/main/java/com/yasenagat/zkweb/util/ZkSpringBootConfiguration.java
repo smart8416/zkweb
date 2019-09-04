14c5e41b80d8eab4cb7a138542adf261034d80aa
dfw <git@116.62.40.165:/data/git/tb_web/tb_webBack>
2019-09-04 18:09:59 +0800

    docs: 待完善

--- src/main/java/com/yasenagat/zkweb/util/ZkSpringBootConfiguration.java
+++ src/main/java/com/yasenagat/zkweb/util/ZkSpringBootConfiguration.java
@@ -1,55 +1,20 @@
 package com.yasenagat.zkweb.util;
 
 
-import java.beans.PropertyVetoException;
 import java.util.Locale;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 public class ZkSpringBootConfiguration {
 	@Autowired
     private Environment env;
-	
-    @Bean(name = "dataSource")
-    @Qualifier(value = "dataSource")
-    @Primary
-    //@ConfigurationProperties(prefix = "c3p0")
-    public DataSource getDataSource(){
-    	//DruidDataSource dataSource = new DruidDataSource();
-    	ComboPooledDataSource dataSource= org.springframework.boot.jdbc.DataSourceBuilder.create().type(ComboPooledDataSource.class).build();
-    	dataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUser(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        try {
-			dataSource.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
-		} catch (PropertyVetoException e) {
-			e.printStackTrace();
-			return null;
-		}
-        dataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("spring.datasource.initial-pool-size")));
-        dataSource.setMinPoolSize(Integer.parseInt(env.getProperty("spring.datasource.min-pool-size")));
-        dataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("spring.datasource.max-pool-size")));
-        dataSource.setAcquireIncrement(Integer.parseInt(env.getProperty("spring.datasource.acquire-increment")));
-        dataSource.setIdleConnectionTestPeriod(Integer.parseInt(env.getProperty("spring.datasource.idle-connection-test-period")));
-        dataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("spring.datasource.max-idle-time")));
-        dataSource.setMaxStatements(Integer.parseInt(env.getProperty("spring.datasource.max-statements")));
-        dataSource.setAcquireRetryAttempts(Integer.parseInt(env.getProperty("spring.datasource.acquire-retry-attempts")));
-        dataSource.setBreakAfterAcquireFailure(Boolean.parseBoolean(env.getProperty("spring.datasource.break-after-acquire-failure")));
-        return dataSource;
-    }
-    
+
     @Bean
     public SessionLocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
@@ -57,7 +22,7 @@ public class ZkSpringBootConfiguration {
         slr.setDefaultLocale(Locale.CHINA);
         return slr;
     }
-    
+
 //    @Bean
 //    public LocaleChangeInterceptor localeChangeInterceptor() {
 //        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
@@ -65,7 +30,7 @@ public class ZkSpringBootConfiguration {
 //        lci.setParamName("lang");
 //        return lci;
 //    }
-    
+
 //  @Bean
 //  public MessageSource messageSource() {
 //      //logger.info("MessageSource");
@@ -75,7 +40,7 @@ public class ZkSpringBootConfiguration {
 //      return messageSource;
 //  }
 
-  @Bean
+  /*@Bean
   public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
   	RequestMappingHandlerAdapter requestMappingHandlerAdapter=new RequestMappingHandlerAdapter();
   	//spring boot 已经自带
@@ -87,8 +52,8 @@ public class ZkSpringBootConfiguration {
 //  	requestMappingHandlerAdapter.setMessageConverters(Lists.newArrayList(stringHttpMessageConverter,
 //  			mappingJackson2HttpMessageConverter));
   	return requestMappingHandlerAdapter;
-  }
-  
+  }*/
+
   //相当于 spring.mvc.servlet.load-on-startup=1
 //@Bean
 //public static BeanFactoryPostProcessor beanFactoryPostProcessor() {
